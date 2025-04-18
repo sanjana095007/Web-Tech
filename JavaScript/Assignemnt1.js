db5e1823ac4e016837e1d32b5abd13ea868529a0
sanjana095007 <sanjanaudgimat@gmail.com>
2025-04-18 18:21:05 +0530

    "uploading CSS"

---
+++ JavaScript/Assignemnt1.js
@@ -0,0 +1,44 @@
+var name='sanj';
+var name='sanj';
+var name=2344;
+var name='raj'
+
+let names='raj';
+
+names=raj; 
+
+const pi=3.14;
+
+pi=3.134;
+
+var age=10;
+
+age=109;
+
+let gender='male'
+
+gender='female'
+
+const mobile='samsung'
+
+mobile='iphone'
+
+document.writeln(age)
+
+let emp1={id:12,employeename:'sanjana',salary:3456}
+
+let emp2=new Object();
+
+emp2.id=14;
+emp2.employeename='raj';
+emp2.salary=5623;
+emp2.id=20;
+
+function emp3(id,employeename,salary){
+    this.id=id;
+    this.employeename=employeename;
+    this.salary=salary;
+}
+
+let obj=emp3(satisfies,satisfies,3);
+
